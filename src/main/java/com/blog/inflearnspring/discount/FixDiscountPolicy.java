a7317053983ea270efedb08c8959bf6682fc56a6
Chanseong <reachtherecord@gmail.com>
2023-01-21 22:16:12 +0900

    @Primary
    Primary는 기본값처럼 동작,
    Qualifier는 매우 상세하게 동작함
    자동보다는 수동이 우선순위가 높으므로 Qualifier가 Primary보다 우선권 높음

--- src/main/java/com/blog/inflearnspring/discount/FixDiscountPolicy.java
+++ src/main/java/com/blog/inflearnspring/discount/FixDiscountPolicy.java
@@ -2,9 +2,7 @@ package com.blog.inflearnspring.discount;
 
 import com.blog.inflearnspring.member.Grade;
 import com.blog.inflearnspring.member.Member;
-import org.springframework.beans.factory.annotation.Qualifier;
 
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; //1000원 할인
 
--- src/main/java/com/blog/inflearnspring/order/OrderServiceImpl.java
+++ src/main/java/com/blog/inflearnspring/order/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import com.blog.inflearnspring.member.Member;
 import com.blog.inflearnspring.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
