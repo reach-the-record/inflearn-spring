14c7093ed5d4df6671ca2a6c1bcc0fd6a1e4aa22
Chanseong <reachtherecord@gmail.com>
2023-01-23 02:00:35 +0900

    Initializing/DisposableBean interface
    스프링 전용 인터페이스.
    초창기에 나온 방법들이며 거의 사용하지 않으나, 알아두면 좋음

--- src/test/java/com/blog/inflearnspring/lifecycle/NetworkClient.java
+++ src/test/java/com/blog/inflearnspring/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package com.blog.inflearnspring.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-    }
+     }
 
     public void setUrl(String url) {
         this.url = url;
@@ -23,5 +24,16 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("disconnected: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
